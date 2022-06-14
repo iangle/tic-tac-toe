801a50c2bf556dc947c123602d18dc308559de5b
Isaac Angle <isaacangle@Isaacs-MacBook-Pro.local>
2022-06-14 13:13:56 -0700

    make project stucture work better for heroku

--- backend/user-functions/autoConfirmUser.py
+++
@@ -1,6 +0,0 @@
-def lambda_handler(event, context):
-    # It sets the user pool autoConfirmUser flag after validating the email domain
-    event['response']['autoConfirmUser'] = True
-
-    # Return to Amazon Cognito
-    return event
